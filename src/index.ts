a7fad20585c6d995ac80f14393b3cc87c5352f5b
GoatSeller <goatseller@outlook.com>
2019-11-07 19:08:18 +0100

    Upload #4

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,29 @@ let app: express.Application = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('./views'));
+
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const err: any = new Error('Not found');
+    err.status = 404;
+    next(err);
+  }
+);
+
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message
+    });
+  }
+);
+
 app.use(router);
 
 app.listen(8000, () => {
